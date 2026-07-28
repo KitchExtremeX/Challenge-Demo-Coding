8d37561f46445bbcd20b31045411506dd41dc2d4
Kadien Coe <kscoe@memphis.edu>
2026-07-28 17:07:01 -0500

    Delete create.js

--- create.js
+++
@@ -1,7 +0,0 @@
-// This function triggers the alert greeting
-function showGreeting() {
-    alert("Hello! Welcome to my first working website.");
-}
-
-// Optional: Run automatically when the file loads
-showGreeting();
